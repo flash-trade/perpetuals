8f46bd63bdd90b345d4422b752602035d4df583c
RustBlockchainEngineer <topstack2021@gmail.com>
2023-04-24 13:51:27 -0700

    Add: comment

--- programs/limit-order/src/instructions/user/cancel_order.rs
+++ programs/limit-order/src/instructions/user/cancel_order.rs
@@ -102,7 +102,7 @@ pub fn cancel_order(ctx: Context<CancelOrder>, params: &CancelOrderParams) -> Re
     // todo: write code to remove collateral from total_amounts_data of limi_order
 
     // we can add `remove collateral` before close_position
-    
+
     perpetuals::cpi::close_position(
         ctx.accounts.to_close_position_context(),
         ClosePositionParams {
